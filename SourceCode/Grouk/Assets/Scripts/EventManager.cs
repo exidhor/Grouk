849b47f5ff2487b0b07b0c6e02b2431c0818eba2
Jipay <jipay.detiege@gmail.com>
2017-02-05 15:47:59 +0100

    Create the first prototype

---
+++ SourceCode/Grouk/Assets/Scripts/EventManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Grouk
+{
+    public class EventManager : MonoBehaviour
+    {
+        public void OnWizardTowerSelected()
+        {
+            Debug.Log("Wizard Tower Selected");
+        }
+
+        public void OnSignSelected()
+        {
+            Debug.Log("Sign Selected");
+        }
+    }
+}
